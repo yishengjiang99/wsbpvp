4a9f0884b0d507bada76d5b4dd97be39fd2ec4ee
Yisheng Jiang <yisheng@#foglogic.com>
2018-12-09 01:01:16 -0800

    fixed dot ticking logic

--- dps_rotation/rogues.php
+++ dps_rotation/rogues.php
@@ -7,7 +7,23 @@
 
 
     function simulateCastSqeuence($sequence,$ntime=20){
-        require_once("rogue_spell_info.php");
+        $spells=['mutilate','garrotte','rupture','envenom','toxicblade'];
+        $initial_damage=[2000,0,0,6000,4000];
+        $damage_over_time=[0,400,500,0,0];
+        $dot_frequencies=[0,3,3,0,0];
+        $dot_tick_energy_regen=[0,7,7,0,0];    
+        $initial_damage_combo_point_regen=[2,1,0,0,0,1];
+        $initial_damage_combo_point_regen_with_shrouded_suffocation=[0,2,0,0,0,0];
+        $spell_energy_cost=[30,30,30,30,30];
+        $spell_cp_cost=[0,0,5,5,0];
+        $dot_default_duration=[0,18,8,0,0];
+        $dot_duration_per_cp_spent=[0,0,3,0,0];
+        $spell_additional_damage_per_cp_spent=[0,0,0,100,0];
+        $spell_cooldown = [1,6,1,1,25];
+        $max_energy=150;
+        $initial_energy=$max_energy;
+                
+        
         $t=0;
         $spell_last_cast =[];
         $default_energy_regen=10;
@@ -28,10 +44,10 @@
                 if($dot['remaining_duration'] > 0){
                     $dot['remaining_duration']--;
                 }else{
-
                     continue;
                 }
-                $dot_ticking = $t % $dot_frequency === 0;
+                $dot_frequency = $dot_frequencies[$spell];
+                $dot_ticking = $dot_frequency!==0 && $t % $dot_frequency === 0;
                 if($dot_ticking) {
                     $spell_name = $spells[$spell];
                     echo "\nDot tick on $spell_name";
